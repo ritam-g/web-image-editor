7fb4ff12a6904c734b1d5a9d9a6641a918cedd81
ritam-g <ritammaty2006@gmail.com>
2025-12-24 20:03:04 +0530

    finshing the ui

---
+++ script.js
@@ -0,0 +1,84 @@
+const filters = {
+  brightness: {
+    value: 100,
+    min: 0,
+    max: 200,
+    unit:"%"
+  },
+  contrast: {
+    value: 100,
+    min: 0,
+    max: 200,
+    unit:"%"
+  },
+  exposure: {
+    value: 100,
+    min: 0,
+    max: 200,
+    unit:"%"
+  },
+  saturation: {
+    value: 100,
+    min: 0,
+    max: 200,
+    unit:"%"
+  },
+  hueRotation: {
+    value: 0,
+    min: 0,
+    max: 360,
+    unit:"deg"
+  },
+  blur: {
+    value: 0,
+    min: 0,
+    max: 50,
+    unit:"px"
+  },
+  grayscale: {
+    value: 0,
+    min: 0,
+    max: 100,
+    unit:"%"
+  },
+  sepia: {
+    value: 0,
+    min: 0,
+    max: 100,
+    unit:"%"
+  },
+  opacity: {
+    value: 100,
+    min: 0,
+    max: 100,
+    unit:"%"
+  },
+  invert: {
+    value: 0,
+    min: 0,
+    max: 100,
+    unit:"%"
+  }
+};
+function createFilter(name,unit='%',value,min,max) {
+    const filter=document.createElement('div');
+    filter.classList.add('filter');
+    const input=document.createElement('input');
+    input.setAttribute('type','range');
+    input.setAttribute('min',min);
+    input.setAttribute('max',max);
+    input.setAttribute('value',value);
+    input.setAttribute('data-unit',unit);
+    const p=document.createElement('p');
+    p.innerText=name;
+    filter.appendChild(p);
+    filter.appendChild(input);
+    return filter;
+}
+
+Object.keys(filters).forEach(key=>{
+    
+    const filter=createFilter(key,filters[key].unit,filters[key].value,filters[key].min,filters[key].max);
+    document.querySelector('.filters').appendChild(filter);
+    
+})
