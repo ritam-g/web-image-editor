f5e3455e9e3ba7166aa19ead64f8fabc23b16519
ritam-g <ritammaty2006@gmail.com>
2025-12-26 20:32:46 +0530

    the filter funtionality is added but there is something has bug that we have to fix some funtionality is not working properly

--- script.js
+++ script.js
@@ -11,12 +11,7 @@ const filters = {
     max: 200,
     unit:"%"
   },
-  exposure: {
-    value: 100,
-    min: 0,
-    max: 200,
-    unit:"%"
-  },
+  
   saturation: {
     value: 100,
     min: 0,
@@ -63,6 +58,9 @@ const filters = {
 const imgeCanvas=document.querySelector("#image-canvase")
 const ctx=imgeCanvas.getContext("2d");//! it will give the pen 
 const imageSelect=document.querySelector("#image-input")
+const placeholder=document.querySelector(".placeholder")
+placeholder.style.visibility="visible";
+let image=null;
 function createFilter(name,unit='%',value,min,max) {
     const filter=document.createElement('div');
     filter.classList.add('filter');
@@ -72,6 +70,14 @@ function createFilter(name,unit='%',value,min,max) {
     input.setAttribute('max',max);
     input.setAttribute('value',value);
     input.setAttribute('data-unit',unit);
+    input.addEventListener("input",function (data) {
+    
+      filters[name].value=data.target.value
+      
+      applyFilter(name)
+    
+    })
+    
     const p=document.createElement('p');
     p.innerText=name;
     filter.appendChild(p);
@@ -80,21 +86,32 @@ function createFilter(name,unit='%',value,min,max) {
 }
 
 Object.keys(filters).forEach(key=>{
-    
     const filter=createFilter(key,filters[key].unit,filters[key].value,filters[key].min,filters[key].max);
     document.querySelector('.filters').appendChild(filter);
-    
 })
 imageSelect.addEventListener("change", (data) => {
   const file=data.target.files[0];
   const img=new Image();
   img.src=URL.createObjectURL(file);
+  
   img.onload=()=>{
+    image=img//! HER IMAGE IS 100% READY SO HERE SHOULD STORE THE DATA  
     imgeCanvas.width=img.width;
     imgeCanvas.height=img.height;
     ctx.drawImage(img,0,0);
   }
+  placeholder.style.display="none";
 });
+
+function applyFilter(filterName) {
+  if (!image) {
+    return
+  }
+  ctx.filter=`${filterName}(${filters[filterName].value}${filters[filterName].unit})`;
+  ctx.clearRect(0,0,imgeCanvas.width,imgeCanvas.height)
+  ctx.drawImage(image, 0, 0)
+}
+
 
 
 
